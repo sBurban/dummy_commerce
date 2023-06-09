56ace3b7ae070bdb7ab7e40db40ffb631c3d0d70
sBurban <sebburbano@gmail.com>
2023-06-09 10:52:46 -0500

    finished configuring DB and 'seeder' methods

---
+++ src/data/users_seeder.js
@@ -0,0 +1,37 @@
+const users = [
+    {
+        "id": 1,
+        "email":"test@testing.com",
+        "name":"fulanito perez",
+        "image":"https://goat.com.co/assets/images/icon.svg",
+        "password":"password",
+        "username":"tester",
+        "first_name": "fulanito",
+        "last_name": "perez",
+        "telephone": "0123456789"
+    },
+    {
+        "id": 2,
+        "email":"johndoe@testing.com",
+        "name":"John Doe",
+        "image":"https://www.pngkey.com/png/detail/400-4000020_some-random-guy-photograph.png",
+        "password":"password",
+        "username":"johny",
+        "first_name": "john",
+        "last_name": "doe",
+        "telephone": "9874561230"
+    },
+    {
+        "id": 3,
+        "email":"johndoe@testing.com",
+        "name":"Harry Toluka",
+        "image":"https://www.smilemakeoverpro.com/wp-content/uploads/2021/01/porcelain-veneers.png",
+        "password":"password",
+        "username":"tolu",
+        "first_name": "harry",
+        "last_name": "toluka",
+        "telephone": "0159784263"
+    },
+]
+
+export default users;
---
+++ src/lib/productApi.ts
@@ -0,0 +1,60 @@
+import { connectToDatabase } from "./mongodb";
+import parseMongoObj from "../utils/parseMongoObj";
+
+import { ProductType } from "@/common/Types";
+import { TABLE_USERS } from "./dbTables";
+
+import users_seeders from '../data/users_seeder';
+
+// export type ResponseFromServer = {
+//     title: string;
+//     content: string;
+//     _id: string;
+// };
+
+// type ProductsCollectionResponse = {
+//     data: ProductType[] | void[],
+//     error: string | null
+// }
+export async function fetchProductsFromDB(){
+// export async function fetchProductsFromDB():Promise<ProductsCollectionResponse>{
+    try {
+        const {client, db} = await connectToDatabase();
+        const dbCollection = db.collection(TABLE_USERS);
+        const response = await dbCollection.find().toArray();
+        // const products = response.map(product => {
+        const products:ProductType[] = response.map(product => {
+            return parseMongoObj(product)
+        });
+        // console.log("🚀 ~ file: productApi.ts:16 ~ fetchProductsFromDB ~ products:", products)
+
+        return {
+            data: products,
+            error: null
+        }
+
+    } catch (error) {
+        console.log("ERROR GETTING PRODUCTS FROM DATABASE")
+        throw new Error( (error as Error)?.message);
+    }
+}
+
+export async function fetchProductById(productId:string){
+    const queryId = parseInt(productId);
+    try {
+        const {client, db} = await connectToDatabase();
+        const dbCollection = db.collection(TABLE_USERS);
+        const response = await dbCollection.findOne({ id: queryId });
+        const product:ProductType = parseMongoObj(response);
+        console.log("🚀 ~ file: productApi.ts:45 ~ fetchProductById ~ product:", product);
+        return {
+            data: product,
+            error: null
+        }
+
+    } catch (error) {
+        console.log("ERROR GETTING PRODUCT BY ID")
+        throw new Error( (error as Error)?.message);
+    }
+
+}
