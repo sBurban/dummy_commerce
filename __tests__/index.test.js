b9c280fbc9e03bec62cdd1134c5d87870a840793
sBurban <sebburbano@gmail.com>
2023-06-20 21:51:29 -0500

    working on unit tests for 'utils' functions

---
+++ __tests__/index.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import Home from '../src/pages/index';
+
+describe('Home', () => {
+    test('renders a heading', () =>{
+        //Arrange
+        // render(<Home />);
+        // //Act
+        // const heading = screen.queryByText('welcome', { exact: false });
+        // //Assert
+        // expect(heading).not.toBeNull();
+    })
+})
---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom/extend-expect'
+
+import { TextEncoder, TextDecoder } from 'util';
+Object.assign(global, { TextDecoder, TextEncoder });
