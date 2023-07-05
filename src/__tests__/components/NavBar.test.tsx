3d1a87eb80112587ba9d30f7beb933ac4ed6084a
sburban <sebburbano@gmail.com>
2023-07-04 22:24:11 -0500

    finished implementing 'useRouter' and 'useSession' mock for NavBar

--- src/__tests__/components/NavBar.test.tsx
+++ src/__tests__/components/NavBar.test.tsx
@@ -3,99 +3,44 @@ import {render, screen, waitFor} from '@testing-library/react';
 import NavBar from "../../components/menus/NavBar";
 
 //https://github.com/vercel/next.js/discussions/23034 - ghost on Dec 23, 2019
-jest.mock("next/router", () => ({
-    useRouter() {
-      return {
-        route: "",
-        pathname: "",
-        query: "",
-        asPath: "",
-      };
-    },
-  }));
+// jest.mock("next/router", () => ({
+//   useRouter() {
+//     return {
+//       route: "",
+//       pathname: "",
+//       query: "",
+//       asPath: "",
+//     };
+//   },
+// }));
 const useRouter = jest.spyOn(require('next/router'), 'useRouter')
 
-//https://github.com/nextauthjs/next-auth/discussions/4185 - agangdi on Mar 20, 2022
-// jest.mock("next-auth/react", () => {
-//     const originalModule = jest.requireActual('next-auth/react');
-//     const mockSession = {
-//       expires: new Date(Date.now() + 2 * 86400).toISOString(),
-//       user: { username: "admin" }
-//     };
-//     return {
-//       __esModule: true,
-//       ...originalModule,
-//       useSession: jest.fn(() => {
-//         // return {}
-//         // return {data:{}, status: 'unauthenticated'}
-//         return {data: mockSession, status: 'authenticated'}  // return type is [] in v3 but changed to {} in v4
-//       }),
-//     };
-// });
-
-// import { useSession, SessionProvider } from "next-auth/react";
-
-// import * as nextAuthReact from 'next-auth/react';
-import { UseSessionOptions } from "next-auth/react";
-// jest.mock('next-auth/react');
-// const nextAuthReactMocked = nextAuthReact as jest.Mocked<typeof nextAuthReact>;
-import nextAuthReactMocked, {mockUserSession, mockUpdateSession} from "@/lib/utils/testUtils/mockNextAuth";
+import nextAuthReactMocked, {mockUserSession, mockUpdateSessionAuth} from "@/lib/utils/testUtils/mockNextAuth";
 jest.mock('next-auth/react');
 
-// import mockNextAuth from "@/lib/utils/testUtils/mockNextAuth";
-
-
 describe('NavBar component', () => {
+  beforeAll(() => {
+    useRouter.mockImplementation(() => ({
+        route: "/yourRoute",
+        pathname: "/yourRoute",
+        query: "",
+        asPath: "",
+    }));
+  })
     test('renders without errors', () => {
-        useRouter.mockImplementation(() => ({
-            route: "/yourRoute",
-            pathname: "/yourRoute",
-            query: "",
-            asPath: "",
-        }));
         const { container } = render(<NavBar />);
         expect(container).toBeInTheDocument();
     });
 
     test('render static Texts', async () => {
-      // nextAuthReactMocked.useSession.mockImplementation(
-      //   (_options?: UseSessionOptions<boolean> | undefined) => {
-      //     return { update: () => (new Promise((resolve,reject) => resolve(null))), data: null, status: 'loading' };
-      //   }
-      // );
-      // nextAuthReactMocked.signIn.mockImplementation(() =>
-      //   Promise.resolve({ error: '', status: 403, ok: false, url: '' })
-      // );
         const { container } = render(<NavBar />);
-        // expect(container).toBeInTheDocument();
-        // const textElements = await screen.findAllByText("About us", {exact: false});
-        // const expectedElems = textElements.length;
-        // expect(textElements).toHaveLength(expectedElems);
-        await waitFor(() => {
-          const welcomeTitle = screen.getAllByText("About us", {exact: false});
-          expect(welcomeTitle);
-        });
-    })
-
-    test.only('show "sign out" when has session', async () => {
-        nextAuthReactMocked.useSession.mockImplementation(
-          (_options?: UseSessionOptions<boolean> | undefined) => {
-            return { update: mockUpdateSession, data: mockUserSession, status: 'authenticated' };
-          }
-        );
-        const {container} = render(<NavBar/>);
-        const textElements = await screen.findAllByText("Sign Out", {exact: false});
+        expect(container).toBeInTheDocument();
+        const textElements = await screen.findAllByText("About us", {exact: false});
         const expectedElems = textElements.length;
         expect(textElements).toHaveLength(expectedElems);
     })
 
-    test('show "sign in" when No session', async () => {
-
-      // nextAuthReactMocked.useSession.mockImplementation(
-      //   (_options?: UseSessionOptions<boolean> | undefined) => {
-      //     return { update: () => (new Promise((resolve,reject) => resolve(null))), data: null, status: 'loading' };
-      //   }
-      // );
+    test('renders "Sign In" option when No User Session is found', async () => {
         const {container} = render(<NavBar/>);
         const textElements = await screen.findAllByText("Sign In", {exact: false});
         const expectedElems = textElements.length;
@@ -104,6 +49,16 @@ describe('NavBar component', () => {
         //   const welcomeTitle = screen.getAllByText("Sign In", {exact: false});
         //   expect(welcomeTitle);
         // });
+    })
+
+    test('renders "Sign Out" option when User Session is found', async () => {
+        nextAuthReactMocked.useSession.mockImplementation(
+          (_options) => ({ update: mockUpdateSessionAuth, data: mockUserSession, status: 'authenticated' })
+        );
+        const {container} = render(<NavBar/>);
+        const textElements = await screen.findAllByText("Sign Out", {exact: false});
+        const expectedElems = textElements.length;
+        expect(textElements).toHaveLength(expectedElems);
     })
 
 });
--- src/lib/utils/testUtils/mockNextAuth.tsx
+++ src/lib/utils/testUtils/mockNextAuth.tsx
@@ -1,9 +1,8 @@
 import * as nextAuthReact from 'next-auth/react';
 import { UseSessionOptions } from "next-auth/react";
+import { Session } from 'next-auth';
 // jest.mock('next-auth/react');
 const nextAuthReactMocked = nextAuthReact as jest.Mocked<typeof nextAuthReact>;
-
-export const mockUpdateSession = () => (new Promise((resolve,reject) => resolve(null)));
 
 export const mockUserSession = {
   expires: new Date(Date.now() + 2 * 86400).toISOString(),
@@ -11,40 +10,42 @@ export const mockUserSession = {
     name: "admin"
   }
 };
+export const mockUpdateSessionNull = () => new Promise<null>((resolve,reject) => resolve(null));
+export const mockUpdateSessionAuth = () => new Promise<Session>((resolve,reject) => resolve(mockUserSession));
 
+// export const mockUseSessionNull = { update: mockUpdateSessionNull, data: null, status: 'loading' };
+// export const mockUseSessionAuth =  { update: mockUpdateSessionAuth, data: mockUserSession, status: 'authenticated' };
+
+// Default Implementation
+// If a Session is implemented in Test file, will need to re-implement Null session manually
 nextAuthReactMocked.useSession.mockImplementation(
-  (_options?: UseSessionOptions<boolean> | undefined) => {
-    return { update: () => (new Promise((resolve,reject) => resolve(null))), data: null, status: 'loading' };
-  }
+  (_options?: UseSessionOptions<boolean> | undefined) =>
+    ({ update: mockUpdateSessionNull, data: null, status: 'loading' })
 );
+
+
+// nextAuthReactMocked.signIn.mockImplementation(() =>
+//   Promise.resolve({ error: '', status: 403, ok: false, url: '' })
+// );
 
 export default nextAuthReactMocked;
 
 
 /********************* */
-// const originalModule = jest.requireActual('next-auth/react');
-
-// const userSession = {
-//   expires: new Date(Date.now() + 2 * 86400).toISOString(),
-//   user: {
-//     name: "admin"
-//     // username: "admin"
-//   }
-// };
-
-// export const mockNextAuth = jest.mock("next-auth/react", () => {
-//   return {
-//     __esModule: true,
-//     ...originalModule,
-//     useSession: jest.fn(() => {
-//       // return {}
-//       // return {data:{}, status: 'unauthenticated'}
-//       return {
-//         data: userSession,
-//         status: 'authenticated'
-//       }  // return type is [] in v3 but changed to {} in v4
-//     }),
-//   };
+//https://github.com/nextauthjs/next-auth/discussions/4185 - agangdi on Mar 20, 2022
+// jest.mock("next-auth/react", () => {
+//     const originalModule = jest.requireActual('next-auth/react');
+//     const mockSession = {
+//       expires: new Date(Date.now() + 2 * 86400).toISOString(),
+//       user: { username: "admin" }
+//     };
+//     return {
+//       __esModule: true,
+//       ...originalModule,
+//       useSession: jest.fn(() => {
+//         // return {}
+//         // return {data:{}, status: 'unauthenticated'}
+//         return {data: mockSession, status: 'authenticated'}  // return type is [] in v3 but changed to {} in v4
+//       }),
+//     };
 // });
-
-// export default mockNextAuth;
